c60006ede8a81180d094a5859dbc8a3598aa55fd
dzhaniivanov94 <68897146+dzhaniivanov94@users.noreply.github.com>
2023-07-11 18:30:56 +0300

    start implementing products entity

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -33,6 +33,11 @@ const MainNav = ({
       active: pathname === `/${params.storeId}/colors`
     },
     {
+      href: `/${params.storeId}/products`,
+      label: "Products",
+      active: pathname === `/${params.storeId}/products`
+    },
+    {
       href: `/${params.storeId}/categories`,
       label: "Categories",
       active: pathname === `/${params.storeId}/categories`
